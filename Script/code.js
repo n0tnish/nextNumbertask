222efed5629b55ddc2933a4afe8c7fdfa868eef1
n0tnish <nishaatgafieldien@gmail.com>
2023-11-27 10:18:19 +0200

    JS code changed

--- Script/code.js
+++ Script/code.js
@@ -1,13 +1,15 @@
-const numbers = ['1948', '15000', '14800', '75', '63543','31227' ];
+
+const elements = ['1948', '15000', '14800', '75', '63543', '31227'];
 let currentIndex = 0;
 
 function displayNextElement() {
-    let outputElement = document.getElementById('output');
+  let outputElement = document.getElementById("output");
 
-    if (currentIndex < elements.length -1) {
-        currentIndex++;
-    } else {
-        currentIndex = 0;
-    }
-    outputElement.innerHTML = elements[currentIndex];
+  if (currentIndex < elements.length - 1) {
+    currentIndex++; 
+  } else {
+    currentIndex = 0; 
+  }
+
+  outputElement.innerHTML = elements[currentIndex];
 }
